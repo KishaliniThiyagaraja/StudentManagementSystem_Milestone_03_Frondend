7aedfa3c5028bc6f768e13b1f65698fb20b69180
UT01117 <ut01117tic2024@gmail.com>
2024-11-21 15:52:35 +0530

    TimeTable Update

---
+++ src/app/time-table.service.ts
@@ -0,0 +1,34 @@
+import { TimeTable } from './../model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimeTableService {
+
+  url = 'https://localhost:7229/api/TimetableControllers';
+
+  constructor(private http: HttpClient) { }
+
+  getTables(){
+    return this.http.get<TimeTable[]>(this.url);
+  }
+
+  createTable(timeTable:TimeTable){
+    return this.http.post(this.url,timeTable);
+  }
+
+  deleteTable(Id:number){
+    return this.http.delete(this.url + '/' + Id);
+  }
+
+  getTable(Id:number){
+    return this.http.get<TimeTable>(this.url + '/' + Id);
+  }
+
+  updateTable(timeTable:TimeTable, Id:number){
+    return this.http.put(this.url +'/'+ Id,timeTable)
+  }
+}
---
+++ src/model.ts
@@ -0,0 +1,9 @@
+export interface TimeTable {
+  id: number;
+  CourseId: string;
+  Date: string;
+  StartTime: string;
+  EndTime: string;
+  Location:string;
+
+}
