77521eed0133f2426e5965d9a40dd4982b4e7475
UT01117 <ut01117tic2024@gmail.com>
2024-11-22 16:44:46 +0530

    Updates

--- src/app/Components/manager/time-table/time-table-upsert/time-table-upsert.component.ts
+++ src/app/Components/manager/time-table/time-table-upsert/time-table-upsert.component.ts
@@ -2,7 +2,7 @@ import { TimeTableService } from './../../../../time-table.service';
 import { TimeTable } from './../../../../../model';
 
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Route, Router } from '@angular/router';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
@@ -16,6 +16,7 @@ export class TimeTableUpsertComponent implements OnInit {
   timeTables: TimeTable[] = [];
 
   timeTableForm: FormGroup;
+  timeTableEntries: TimeTableEntry[] = [];
 
   constructor(
     private fb: FormBuilder,
@@ -23,18 +24,30 @@ export class TimeTableUpsertComponent implements OnInit {
     private router: Router
   ) {
     this.timeTableForm = this.fb.group({
-      course: [''],
-      startTime: [''],
-      endTime: [''],
-      dueDate: [''],
-      location : ['']
+      course: ['', Validators.required],
+      dueDate: ['', Validators.required],
+      day: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+      class: ['', Validators.required],
     });
   }
 
   onSubmit() {
     let timeTable = this.timeTableForm.value;
-    console.log(this.timeTableForm.value)
-    console.log(timeTable);
+    // console.log(this.timeTableForm.value)
+    // console.log(timeTable);
+    const newEntry: TimeTableEntry = {
+      course: timeTable.course,
+      dueDate: timeTable.dueDate,
+      day: timeTable.day,
+      startTime: timeTable.startTime,
+      endTime: timeTable.endTime,
+      classType: timeTable.class
+    };
+    this.timeTableEntries.push(newEntry); // Add to the array
+      this.timeTableForm.reset(); // Reset the form
+
     this.timeTableService.createTable(timeTable)
     .subscribe((data: any) => {
      // this.router.navigate(['/timetableview']);
@@ -50,4 +63,13 @@ export class TimeTableUpsertComponent implements OnInit {
    })
 
   }
+}
+
+export interface TimeTableEntry {
+  course: string;
+  dueDate: string;
+  day: string;
+  startTime: string;
+  endTime: string;
+  classType: string;
 }
