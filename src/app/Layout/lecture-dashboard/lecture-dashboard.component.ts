d76a2d51dcb9ebe22408ce75d0e17ec34b428c98
Ashan <ut03301tic2024@gmail.com>
2024-12-03 17:09:59 +0530

    Deleted lecture spec file and put router links

--- src/app/Layout/lecture-dashboard/lecture-dashboard.component.ts
+++ src/app/Layout/lecture-dashboard/lecture-dashboard.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lecture-dashboard',
-  templateUrl: './lecture-dashboard.component.html',
-  styleUrl: './lecture-dashboard.component.css'
+  templateUrl:'./lecture-dashboard.component.html',
+  styleUrl:'./lecture-dashboard.component.css'
 })
 export class LectureDashboardComponent {
 
