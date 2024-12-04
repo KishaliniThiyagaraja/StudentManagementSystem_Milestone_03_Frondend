46276bd2521bdc2c3377841ea04e148a7b7a76b1
Prem Arththy <arththyprem@gmail.com>
2024-12-04 08:05:45 +0530

    enrollment

--- src/app/Components/manager/enrollment/enrollment-upsert/enrollment-upsert.component.ts
+++ src/app/Components/manager/enrollment/enrollment-upsert/enrollment-upsert.component.ts
@@ -1,35 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../../../../Services/course.service';
 
 @Component({
   selector: 'app-enrollment-upsert',
   templateUrl: './enrollment-upsert.component.html',
   styleUrl: './enrollment-upsert.component.css'
 })
-export class EnrollmentUpsertComponent {
+// export class EnrollmentUpsertComponent {
+
+//   newEntry = {
+//   name: '',
+//   date: '',
+//   course: '',
+// };
+
+// courses = ['UT0001', 'UT0002', 'UT0003'];
+
+// rows: { name: string; date: string; course: string }[] = [];
+
+// addRow() {
+//   const { name, date, course } = this.newEntry;
+
+//   if (name && date && course) {
+//     this.rows.push({ name, date, course });
+
+//     this.newEntry = { name: '', date: '', course: '' };
+//   } else {
+//     alert('Please fill in all fields!');
+//   }
+// }
+
+// deleteRow(index: number) {
+//   this.rows.splice(index, 1);
+// }
+// }
+export class EnrollmentUpsertComponent implements OnInit {
+deleteRow(_t37: number) {
+throw new Error('Method not implemented.');
+}
 
   newEntry = {
-  name: '',
-  date: '',
-  course: '',
-};
+    name: '',
+    date: '',
+    course: ''
+  };
 
-courses = ['UT0001', 'UT0002', 'UT0003'];
+  courses: { id: string; courseName: string }[] = [];
+  rows: { name: string; date: string; course: string }[] = [];
 
-rows: { name: string; date: string; course: string }[] = [];
+  constructor(private courseService: CourseService) {}
 
-addRow() {
-  const { name, date, course } = this.newEntry;
-
-  if (name && date && course) {
-    this.rows.push({ name, date, course });
-
-    this.newEntry = { name: '', date: '', course: '' };
-  } else {
-    alert('Please fill in all fields!');
+  ngOnInit(): void {
+    this.loadCourses();
   }
-}
 
-deleteRow(index: number) {
-  this.rows.splice(index, 1);
-}
+  loadCourses(): void {
+    this.courseService.getCourse().subscribe(
+      (data) => {
+        this.courses = data;
+      },
+      (error) => {
+        console.error('Error fetching courses:', error);
+      }
+    );
+  }
+
+  addRow(): void {
+    const { name, date, course } = this.newEntry;
+
+    if (name && date && course) {
+      this.rows.push({ name, date, course });
+      this.newEntry = { name: '', date: '', course: '' };
+    } else {
+      alert('Please fill in all fields!');
+    }
+  }
 }
